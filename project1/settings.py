ff9df30625bd8096edc3bad34e1c337ca4edbdab
Esha-23 <esha.tiwari8@gmail.com>
2022-09-12 17:59:04 +0530

    third commit

--- project1/settings.py
+++ project1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-qv5m(st_w(_z#an@r*evu2(wctj2l+@2r3#484@bytccqqdo@m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
